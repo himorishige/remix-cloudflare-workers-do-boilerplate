93f66046ff55fec68adc66780ad848fd858fd785
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-05-19 19:04:54 +0900

    add: counter-do implemet

--- apps/remix-app/app/root.tsx
+++ apps/remix-app/app/root.tsx
@@ -1,12 +1,17 @@
-import type { MetaFunction } from '@remix-run/cloudflare';
+import type { LoaderFunction, MetaFunction } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
+  useMatches,
 } from '@remix-run/react';
+import type { PropsWithChildren } from 'react';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -14,19 +19,87 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
-export default function App() {
+type LoaderData = {
+  loaderCalls: number;
+};
+
+export let loader: LoaderFunction = async ({ context: { env } }) => {
+  let counter = env.COUNTER.get(env.COUNTER.idFromName('root'));
+  let counterResponse = await counter.fetch('https://.../increment');
+  let loaderCalls = Number.parseInt(await counterResponse.text());
+
+  return json<LoaderData>({ loaderCalls });
+};
+
+function Document({ children }: PropsWithChildren<{}>) {
+  let matches = useMatches();
+  let root = matches.find((match) => match.id === 'root');
+  let data = root?.data as LoaderData | undefined;
+
   return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@exampledev/new.css@1.1.3/new.css"
+        />
       </head>
       <body>
-        <Outlet />
+        <header>
+          <h1>
+            <Link to="/">Remix Chat</Link>
+          </h1>
+          <p>
+            This chat runs entirely on the edge, powered by Cloudflare Workers
+            Durable Objects
+          </p>
+        </header>
+        {children}
+        {data && (
+          <>
+            <hr />
+            <footer>root loader invocations: {data.loaderCalls}</footer>
+          </>
+        )}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let { status, statusText } = useCatch();
+
+  return (
+    <Document>
+      <main>
+        <h1>{status}</h1>
+        {statusText && <p>{statusText}</p>}
+      </main>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.log(error);
+
+  return (
+    <Document>
+      <main>
+        <h1>Oops, looks like something went wrong 😭</h1>
+      </main>
+    </Document>
   );
 }
--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -1,32 +1,74 @@
+import type { LoaderFunction } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
+import { Form, useLoaderData } from '@remix-run/react';
+
+import { getSession } from '~/session.server';
+
+type LoaderData = {
+  loaderCalls: number;
+  username?: string;
+};
+
+export let loader: LoaderFunction = async ({ context: { env }, request }) => {
+  let sessionPromise = getSession(request, env);
+
+  let counter = env.COUNTER.get(env.COUNTER.idFromName('index'));
+  let loaderCalls = await counter
+    .fetch('https://.../increment')
+    .then((response) => response.text())
+    .then((text) => Number.parseInt(text, 10));
+
+  let session = await sessionPromise;
+  let username = (session.get('username') || undefined) as string | undefined;
+
+  return json<LoaderData>({ loaderCalls, username });
+};
+
 export default function Index() {
+  let { loaderCalls, username } = useLoaderData() as LoaderData;
+
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <Form method="post" id="username-form">
+        <label>
+          Choose a Username:
+          <br />
+          <input
+            name="username"
+            placeholder="username"
+            required
+            maxLength={32}
+            defaultValue={username}
+          />
+        </label>
+        <button disabled type="submit" style={{ display: 'none' }} />
+      </Form>
+      <p>then</p>
+      <p>
+        <button type="submit" form="username-form" formAction="/new">
+          Create a Private Room
+        </button>
+      </p>
+      <p>or</p>
+      <label>
+        Enter a Public Room:
+        <br />
+        <input
+          form="username-form"
+          name="room"
+          type="text"
+          autoCapitalize="off"
+          placeholder="room-name"
+        />
+      </label>
+      <button type="submit" form="username-form" formAction="/join">
+        GO!
+      </button>
+
+      <hr />
+      <footer>
+        <p>index loader invocations: {loaderCalls}</p>
+      </footer>
+    </main>
   );
 }
